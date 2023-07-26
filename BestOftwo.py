b2b1228c119e3fa350f9db44bd5efb085d27bb6a
Dhruvabhat24 <dhruvabhat24@gmail.com>
2023-07-26 18:17:45 +0530

    Program2A

---
+++ BestOftwo.py
@@ -0,0 +1,8 @@
+print("Enter the test marks:")
+test1=float(input("Test 1 marks:"))
+test2=float(input("Test 2 marks:"))
+test3=float(input("Test 3 marks:"))
+average1=(test1+test2)/2
+average2=(test2+test3)/2
+average3=(test1+test3)/2
+print(max(average1,average2,average3))
